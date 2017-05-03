945b706bb19dc43ded69cc1204f66c363230555c
wudifeixue <wudifeixue@users.noreply.github.com>
2017-05-02 21:56:11 -0600

    Add components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Create a new component. This component should produce some HTML. 
-const App = function() {
+const App = () => {
   return <div>Hello World!</div>;
 }
 // Take this component's generated HTML and put it on the page (in the DOM)
