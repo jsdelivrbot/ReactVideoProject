34ba05d0af93aca6c21f109f04d7fb367fc8d5d3
wudifeixue <wudifeixue@users.noreply.github.com>
2017-05-03 21:02:53 -0600

    Add search component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search-bar'
+
 const YOUTUBE_API_KEY = 'AIzaSyAP6i-VyWyJulmdAIvASVzW26J6avY3Z5c';
 // Create a new component. This component should produce some HTML. 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 // Take this component's generated HTML and put it on the page (in the DOM)
 
